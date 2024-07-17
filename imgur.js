b7f2151a36461970bcd10eefeab3479a2409e5ce
Ferry Ayunda <165256908+jiinbe@users.noreply.github.com>
2024-07-17 23:52:27 +0700

    Create imgur.js

---
+++ imgur.js
@@ -0,0 +1,38 @@
+const imgfile = document.querySelector("#input-file");
+
+imgfile.addEventListener("change", (event) => {
+const selectedFile = event.target.files[0];
+// FormData
+const formData = new FormData();
+formData.append("image", selectedFile);
+
+// config
+const config = {
+headers: {
+Authorization: "Client-ID 4ddb1cef2cbd23a", // YOUR_CLIENT_ID Imgur
+},
+};
+
+// POST
+axios
+.post("https://api.imgur.com/3/image", formData, config)
+.then((response) => {
+console.log(response.data);
+// link
+const { link } = response.data.data;
+console.log(link);
+const preview = document.querySelector("#placeholder");
+preview.src = link;
+const imgLink = document.querySelector("#url");
+imgLink.textContent = link;
+})
+.catch((error) => {
+console.error(error);
+// error
+alert("error!");
+});
+});
+
+function selectFile() {
+document.getElementById("input-file").click();
+}
