b8d9740f1fc4524e544d015d626deccf1bf7b099
Ferry Ayunda <165256908+jiinbe@users.noreply.github.com>
2024-07-18 18:46:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -50,3 +50,19 @@ input.files[0].name.replace(/[^\\\/]*$/, 'avatar.png')
 }
 
 placeholder.onload = drawImageFromInput;
+
+// input check
+
+let clickButton = document.getElementById("dl");
+let fileInput = document.getElementById("Input-file");
+fileInput.addEventListener("change", function () {
+         
+// check if the file is selected or not
+if (fileInput.files.length == 0) {
+clickButton.disabled = true;
+clickButton.opacity = 0.3;
+} else {
+clickButton.disabled = false;
+clickButton.style.opacity = 1;
+}
+});
