c787399180e3953cdad79348a10987ccef9843cc
Ferry Ayunda <165256908+jiinbe@users.noreply.github.com>
2024-07-21 13:58:42 +0700

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,37 +1,93 @@
-var theCanvas;
+var canvas = null;
+var img = null;
+var img2 = null;
+var img3 = null;
+var img4 = null;
+var img5 = null;
+var img6 = null;
 
-function previewFile() {
-var preview = document.querySelector(".prev");
-var file = document.querySelector('input[type=file]').files[0];
-var reader = new FileReader();
-
-reader.addEventListener("load", function() {
-preview.src = reader.result;
-}, false);
-
-if (file) {
-reader.readAsDataURL(file)
-theCanvas = Caman('.prev')
+function upload() {
+  var g = document.getElementById("input-file");
+img = new SimpleImage(g);
+img2 = new SimpleImage(g);           img3 = new SimpleImage(g);
+img4 = new SimpleImage(g);           img5 = new SimpleImage(g);
+img6 =  new SimpleImage(g);  
+  canvas = document.getElementById("preview");
+  img.drawTo(canvas);
 }
+function clearCanvas(canvass) {
+  var ctx = canvass.getContext("2d");
+  ctx.clearRect(0, 0, canvass.width, canvass.height)
+ }
+function wipeClean() {
+  clearCanvas(canvas);
+}
+/*with image2*/function makeGrey() {
+    for (var pixelG of img2.values()){
+    var avg = (pixelG.getRed()+pixelG.getGreen()+pixelG.getBlue())/3;  
+    pixelG.setRed(avg);
+    pixelG.setGreen(avg);
+    pixelG.setBlue(avg);  
+    }
+wipeClean()
+  img2.drawTo(canvas);
 }
 
-var filters = document.querySelectorAll('.filter')
 
-Array.from(filters).forEach(function(node) {
-node.addEventListener('click', function(e) {
-if (typeof theCanvas === 'undefined') return;
-    
-var button = e.target,
-filter = button.dataset.filter,
-filterName = button.textContent;
-    
-button.textContent = 'Processing...';
-    
-theCanvas.reset();
-theCanvas[filter]();
-    
-return theCanvas.render(function () {
-button.textContent = filterName
-});
-})
-})
+/*with image3*/function yellowImage() {
+for(var pixe of img3.values()){
+  var green = 250;
+  var red = 250;
+  pixe.setRed(red);
+  pixe.setGreen(green);
+
+}  
+wipeclean() 
+  img3.drawTo(canvas);
+
+}
+
+
+
+function doEclipse() {
+for (var pixel of img4.values())  {
+  var B = pixel.getRed();
+  var R = pixel.getBlue();
+pixel.setRed(R);
+  pixel.setBlue(B);
+}
+wipeClean()
+  img4.drawTo(canvas);
+}
+
+function doPurple(){
+  for (var pixx of img5.values()){
+   pixx.setRed(255);
+    pixx.setBlue(255);
+  }
+wipeClean()
+  img5.drawTo(canvas);
+}
+
+function resetImage() {
+  wipeClean()
+  img.drawTo(canvas);
+
+}
+function doRed() {
+for (var pixel of img6.values()){
+  var avge = (pixel.getRed()+pixel.getGreen()+pixel.getBlue()/2);
+  if(avge < 128){
+    pixel.setRed(avge*2);
+    pixel.setGreen(0)
+    pixel.setBlue(0);
+  }
+  if(avge => 128){
+   pixel.setRed(255);
+   pixel.setGreen(avge*2-255);
+   pixel.setBlue(avge*2-255) 
+  }
+}
+  wipeClean()
+  img6.drawTo(canvas);
+}
